ca44265bcef1444148b0aa2231cfcdb7c4839e32
Turner-Tobie <turner.tobie@yahoo.com>
2017-02-13 10:47:57 -0500

    new navbar for apps page

--- js/apps.js
+++ js/apps.js
@@ -13,4 +13,45 @@ jQuery(function($){
       $('.active').removeClass('active'); 
       $(this).addClass('active'); 
     });
+    var count = 0;
+  
+    $('li').hover(
+      function(){
+        $(this).fadeTo('fast', .75)},
+      function(){
+        $(this).fadeTo('fast', 1);
+    })
+
+    $('li').on('click', function(){
+      $(this).return("a")
+    });
+
+    $('.drop-box').on('click', function(){
+      count++;
+
+      var isEven = function(someNumber){return (someNumber % 2 === 0) ? true : false;
+    };   
+   if(isEven(count) === false){
+      $('.nav').animate({
+           height: '+=200px'
+       });
+   } else if (isEven(count) === true){     
+       $('.nav').animate({
+           height: '-=200px'
+       });
+   } 
+      
+  });
+  function glassify(){
+  $('.glass').each( function( index ) {
+    $(this).before("<div class='glass2' id='glass"+index+"'></div>");
+    var h=$(this).outerHeight();
+    $('#glass'+index).css('height', (h) + 'px');
+    var w=$(this).outerWidth();
+    $('#glass'+index).css('width', (w) + 'px');
+    var homeOffset=$(this).offset();
+    $('#glass'+index).offset({ top: homeOffset.top, left: homeOffset.left });
+  });
+};
+
   });
